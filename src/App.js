41d9252503cbd1bf219fa23ba8b3df4a83d9113a
phanimadiraju <98398051+phanimadiraju@users.noreply.github.com>
2022-03-02 15:29:41 +0530

    Changed App to class based component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-
-function App() {
-  return (
-    <div >
-    <div >
-      Phone Directory 
-    </div>
-    <button>Add</button>
-    <span>Name</span><br/>
-    <span>Phone</span>
-    </div>
-  );
+import React,{Component}from 'react';
+class App extends Component {
+  render(){
+    return (
+      <div >
+      <div >
+        Phone Directory 
+      </div>
+      <button>Add</button>
+      <span>Name</span><br/>
+      <span>Phone</span>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
